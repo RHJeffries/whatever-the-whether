94c81889c199d6beebf4935f1d7401f4a2b6c58b
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-10-15 23:27:00 +1030

    search returns result in console

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var APIKey = "5267211d26e021e31b3419800fbf2329";
-var city= [];
-var cityUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var fetchButton = document.getElementById('submit');
+//var city= [];
 
+var fetchButton = document.getElementById('submit');
+var city = document.querySelector('.cityInput');
 
 
 function getCity(){
-    
+    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=5267211d26e021e31b3419800fbf2329";
 fetch(cityUrl)
 .then(function(response){
     return response.json()
